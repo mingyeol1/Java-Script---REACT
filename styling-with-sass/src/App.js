84a59012d610619a7c5bf680bb12e7077940711c
Mingyeol <alsruf741@gmail.com>
2024-06-14 14:31:20 +0900

    2024-06-14 4교시 끝

--- styling-with-sass/src/App.js
+++ styling-with-sass/src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from './component/Button';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='button'>
+        <Button size="large">Button</Button> {/* 하나의 값을 넘길 땐 {}가 의미가 없다. */}
+        <Button  >Button</Button>
+        <Button size={"small"}>Button</Button>
+      </div>
+     
+      <div className='button'>
+        <Button size="large" color="gray">Button</Button> {/* 하나의 값을 넘길 땐 {}가 의미가 없다. */}
+        <Button color="gray">Button</Button>
+        <Button size={"small"} color="gray">Button</Button>
+      </div>
+      
+      <div className='button'>
+        <Button size="large" color="pink">Button</Button> {/* 하나의 값을 넘길 땐 {}가 의미가 없다. */}
+        <Button color="pink">Button</Button>
+        <Button size={"small"} color="pink">Button</Button>
+      </div>
+
+      <div className='button'>
+        <Button size="large" outline>Button</Button> {/* 하나의 값을 넘길 땐 {}가 의미가 없다. */}
+        <Button color="gray" outline>Button</Button>
+        <Button size={"small"} color="pink" outline>Button</Button>
+      </div>
+
+      <div className='button'>
+        <Button size="large" color="blue" fullWidth>Button</Button> {/* 하나의 값을 넘길 땐 {}가 의미가 없다. */}
+        <Button size="large" color="gray" fullWidth>Button</Button>
+        <Button size="large" color="pink" fullWidth>Button</Button>
+      </div>
     </div>
   );
 }
---
+++ styling-with-sass/src/component/Button.js
@@ -0,0 +1,26 @@
+// Button
+import React from 'react';
+import './Button.scss'
+import classNames from 'classnames';
+
+//1. 버튼 생성
+//2. 버튼 사이즈 조정  size가 (large, medium, samll) 있다면
+//3. 버튼 색상 설정하기.  (blue, gray, pink)
+//4. outline 옵션 : 버튼에서 테두리만 보여지게 설정
+//5. fullWidth : 버튼이 전체 너비를 차지하도록 구현
+function Button({children, size, color, outline, fullWidth}){
+    // className='Button large' join을 쓰면 이렇게 만들어짐.
+    // ['Button',size].join(' ') or `Button $(size)` 둘 중 고르셈.
+    //클래스에 관련된 처리를 쉽게하는 라이브러리가 있음 classnames
+    return( 
+    <button className={classNames('Button', size, color, {outline, fullWidth})}>
+        {children}
+    </button>);
+}
+
+Button.defaultProps = { //컴포넌트 프롭스 디폴트값 설정.
+    size: 'medium',
+    color: 'blue'
+};
+
+export default Button;
